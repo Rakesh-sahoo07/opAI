88b3348ea2c5de11d35ba3c2e03572ad55ad1f42
Rakesh-Sahoo77 <rakesh.sahoo@squbix.com>
2024-12-30 17:30:54 +0530

    feat: Update Hardhat configuration to use environment variables for private key

--- hardhat.config.cjs
+++ hardhat.config.cjs
@@ -1,16 +1,17 @@
 require("@nomiclabs/hardhat-waffle");
 require("@nomiclabs/hardhat-ethers");
+require("dotenv").config();
 
 module.exports = {
   solidity: "0.8.26",
   networks: {
     opbnb: {
       url: "https://opbnb-mainnet-rpc.bnbchain.org",
-      accounts: ['af7eefa696da7049974756a31d81b2437a9b27aecb4494cb1d5f90c6db752215']
+      accounts: [process.env.PRIVATE_KEY]
     },
     opbnbTestnet: {
       url: "https://opbnb-testnet-rpc.bnbchain.org",
-      accounts: ['af7eefa696da7049974756a31d81b2437a9b27aecb4494cb1d5f90c6db752215']
+      accounts: [process.env.PRIVATE_KEY]
     }
   }
 };
